fa80dd4a2c021caaab704e0c37f6fb34ecaaa952
giffy1 <giffy1991@gmail.com>
2017-06-20 22:40:01 -0400

    Added additional time format constants, added service message types, removed various unused constants, removed GENERATE_DUMMY_DATA, as data is currently generated now on the initial run. Added a constant for the number of ms in a day. Also added the notification pattern constant.

--- app/src/main/java/cs/umass/edu/customcalendar/constants/Constants.java
+++ app/src/main/java/cs/umass/edu/customcalendar/constants/Constants.java
@@ -11,6 +11,8 @@ public class Constants {
     /** The interval of each minute picker (both AM and PM). **/
     public static final int TIME_PICKER_INTERVAL = 15;
 
+    public static final long[] NOTIFICATION_PATTERN = {0, 600, 0};
+
     public interface DATE_FORMAT {
         SimpleDateFormat AM_PM = new SimpleDateFormat("h:mm a", Locale.getDefault());
         SimpleDateFormat _24_HR = new SimpleDateFormat("H:mm", Locale.getDefault());
@@ -18,6 +20,10 @@ public class Constants {
         SimpleDateFormat MMM_YY = new SimpleDateFormat("MMM yy", Locale.getDefault());
         SimpleDateFormat MMM_YYYY = new SimpleDateFormat("MMM yyyy", Locale.getDefault());
         SimpleDateFormat MONTH_DAY = new SimpleDateFormat("M/dd", Locale.getDefault());
+        SimpleDateFormat MONTH_DAY_YEAR = new SimpleDateFormat("MM-dd-yyyy", Locale.getDefault());
+        SimpleDateFormat YEAR_MONTH_DAY = new SimpleDateFormat("yyy-MM-dd", Locale.getDefault());
+        SimpleDateFormat MONTH_DAY_YEAR_SHORT = new SimpleDateFormat("MM.dd.yy", Locale.getDefault());
+        SimpleDateFormat FULL_AM_PM = new SimpleDateFormat("MM/dd/yyyy h:mm a", Locale.getDefault());
     }
 
     /** Intent actions used to communicate between the main UI and the sensor service
@@ -29,6 +35,14 @@ public class Constants {
         String STOP_SERVICE = "edu.umass.cs.prepare.action.stop-service";
         String QUERY_BATTERY_LEVEL = "edu.umass.cs.prepare.action.query-battery-level";
         String QUERY_CONNECTION_STATE = "edu.umass.cs.prepare.action.query-connection-state";
+        String PILL_INTAKE_RESPONSE_YES = "edu.umass.cs.prepare.action.pill-intake-response-yes";
+        String PILL_INTAKE_RESPONSE_NO = "edu.umass.cs.prepare.action.pill-intake-response-no";
+        String SCHEDULE_REMINDERS = "edu.umass.cs.prepare.action.schedule-reminders";
+    }
+
+    public interface SERVER_MESSAGE {
+        String PILL_INTAKE_GESTURE = "PILL_INTAKE_GESTURE";
+        String UPDATE_DATA = "UPDATE_DATA";
     }
 
     public interface MESSAGES {
@@ -51,39 +65,31 @@ public class Constants {
         int WEARABLE_CONNECTED = 16;
         int WEARABLE_DISCONNECTED = 17;
         int SERVER_CONNECTION_SUCCEEDED = 18;
-        int SERVER_DISCONNECTED =
-                19;
+        int SERVER_DISCONNECTED = 19;
         int WEARABLE_CONNECTION_FAILED = 20;
+        int PILL_INTAKE_GESTURE_DETECTED = 21;
+        int PILL_INTAKE_GESTURE_CONFIRMED = 22;
+        int PILL_INTAKE_GESTURE_DENIED = 23;
     }
 
     /**
      * Keys used to identify data instances from data packages sent over Bluetooth to the mobile device.
      */
     public interface KEY {
-        String UUID = "edu.umass.cs.prepare.key.uuid";
-        String TIMESTAMPS = "edu.umass.cs.prepare.key.timestamps";
-        String VALUES = "edu.umass.cs.prepare.key.sensor-values";
         String MESSAGE = "edu.umass.cs.prepare.key.message";
-
-        String RECORD_AUDIO = "edu.umass.cs.prepare.key.record-audio";
-        String SURFACE_WIDTH = "edu.umass.cs.prepare.key.surface-width";
-        String SURFACE_HEIGHT = "edu.umass.cs.prepare.key.surface-height";
-        String SURFACE_X = "edu.umass.cs.prepare.key.surface-x";
-        String SURFACE_Y = "edu.umass.cs.prepare.key.surface-y";
-        String SENSOR_DATA = "edu.umass.cs.prepare.key.sensor-data";
-        String SENSOR_TYPE = "edu.umass.cs.prepare.key.sensor-type";
-        String TIMESTAMP = "edu.umass.cs.prepare.key.timestamp";
-        String PAGE_INDEX = "edu.umass.cs.prepare.key.page-index";
+        String MEDICATION = "edu.umass.cs.prepare.key.medication";
+        String TIME_TAKEN = "edu.umass.cs.prepare.key.time-taken";
         String NOTIFICATION_ID = "edu.umass.cs.prepare.key.notification-id";
     }
 
     public interface NOTIFICATION_ID {
         int BAND_SERVICE = 101;
         int METAWEAR_SENSOR_SERVICE = 102;
-        int DATA_WRITER_SERVICE = 103;
-        int RECORDING_SERVICE = 104;
     }
 
-    // TODO: Only for debugging purposes
-    public static boolean GENERATE_DUMMY_DATA = true;
+    public interface REQUEST_CODE {
+        int ENABLE_BLUETOOTH = 1;
+    }
+
+    public static int MILLISECONDS_PER_DAY = 24*60*60*1000;
 }
