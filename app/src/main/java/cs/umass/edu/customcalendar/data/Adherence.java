3c1d9eccdd1b25b0152ec3c0265d1b9cd18c907e
giffy1 <giffy1991@gmail.com>
2017-06-20 22:36:30 -0400

    Added documentation. The getTimeTaken() method is nullable, as it is possible that the timestamp is null (for example if the adherence type is none).

--- app/src/main/java/cs/umass/edu/customcalendar/data/Adherence.java
+++ app/src/main/java/cs/umass/edu/customcalendar/data/Adherence.java
@@ -1,10 +1,19 @@
 package cs.umass.edu.customcalendar.data;
 
+import android.support.annotation.Nullable;
+
 import java.io.Serializable;
 import java.util.Calendar;
 
+/**
+ * The adherence class wraps an adherence type, along with a timestamp,
+ * indicating when the pill was taken, if applicable.
+ */
 public class Adherence implements Serializable {
 
+    /**
+     * Indicates the type of adherence.
+     */
     public enum AdherenceType {
         MISSED,
         TAKEN,
@@ -19,26 +28,50 @@ public class Adherence implements Serializable {
         }
     }
 
+    /**
+     * The adherence type.
+     */
     private AdherenceType adherenceType;
+
+    /**
+     * The timestamp at which the pill was taken; null if not applicable.
+     */
     private Calendar timeTaken;
 
-    public Adherence(AdherenceType adherenceType, Calendar timeTaken){
+    public Adherence(AdherenceType adherenceType, @Nullable Calendar timeTaken){
         this.adherenceType = adherenceType;
         this.timeTaken = timeTaken;
     }
 
+    /**
+     * Returns the adherence type.
+     * @return one of {@link AdherenceType}.
+     */
     public AdherenceType getAdherenceType() {
         return adherenceType;
     }
 
+    /**
+     * Returns the timestamp at which the pill was taken.
+     * @return a {@link Calendar} object encoding a timestamp.
+     */
+    @Nullable
     public Calendar getTimeTaken() {
         return timeTaken;
     }
 
+    /**
+     * Sets the adherence type.
+     * @param adherenceType one of {@link AdherenceType}.
+     */
     public void setAdherenceType(AdherenceType adherenceType) {
         this.adherenceType = adherenceType;
     }
 
+    /**
+     * Sets the timestamp at which the pill was taken.
+     * @param timeTaken a {@link Calendar} object encoding a timestamp.
+     */
     public void setTimeTaken(Calendar timeTaken) {
         this.timeTaken = timeTaken;
     }
