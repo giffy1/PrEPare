c4982bba68d6bdb54a9db7b13273f294930aba9c
giffy1 <giffy1991@gmail.com>
2017-06-20 22:18:04 -0400

    When a user maps devices to medications, each device should be configured, such that when motion is detected, it  reconfigures its beaconing parameters to inform the application of motion and of which device UUID. However, it is currently not working as expected and I'm testing it to instead of beaconing turn on/off the LED. It may also be wise to connect to the device directly when motion is detected (that is, easier and can be done with Android Wear), however it's not clear whether Android Wear can even connect to that many devices simultaneously. (immediate disconnect is also an option?).

--- app/src/main/java/cs/umass/edu/customcalendar/view/activities/SelectDeviceActivity.java
+++ app/src/main/java/cs/umass/edu/customcalendar/view/activities/SelectDeviceActivity.java
@@ -1,17 +1,35 @@
 package cs.umass.edu.customcalendar.view.activities;
 
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothDevice;
 import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.database.DataSetObserver;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mbientlab.bletoolbox.scanner.BleScannerFragment.ScannerCommunicationBus;
+import com.mbientlab.bletoolbox.scanner.ScannedDeviceInfo;
+import com.mbientlab.bletoolbox.scanner.ScannedDeviceInfoAdapter;
+import com.mbientlab.metawear.CodeBlock;
 import com.mbientlab.metawear.MetaWearBoard;
+import com.mbientlab.metawear.Route;
 import com.mbientlab.metawear.android.BtleService;
+import com.mbientlab.metawear.module.AccelerometerBosch;
+import com.mbientlab.metawear.module.IBeacon;
+import com.mbientlab.metawear.module.Led;
+import com.mbientlab.metawear.module.Timer;
 
 import java.util.HashMap;
 import java.util.List;
@@ -19,6 +37,8 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.UUID;
 
+import bolts.Continuation;
+import bolts.Task;
 import cs.umass.edu.customcalendar.R;
 import cs.umass.edu.customcalendar.data.Medication;
 import cs.umass.edu.customcalendar.io.ApplicationPreferences;
@@ -51,6 +71,20 @@ public class SelectDeviceActivity extends AppCompatActivity implements ScannerCo
 
     private final static UUID[] serviceUUIDs;
 
+    private BtleService.LocalBinder serviceBinder;
+
+    private MetaWearBoard mwBoard;
+
+    private IBeacon iBeaconModule;
+
+    private Led ledModule;
+
+    private ListView lstDevices;
+
+    private Timer timer;
+
+    private Map<Medication, String> addressMapping = new HashMap<>();
+
     static {
         serviceUUIDs = new UUID[] {
                 MetaWearBoard.METAWEAR_GATT_SERVICE,
@@ -64,12 +98,51 @@ public class SelectDeviceActivity extends AppCompatActivity implements ScannerCo
         setContentView(R.layout.activity_select_metawear);
 
         preferences = ApplicationPreferences.getInstance(this);
-        medications = preferences.getMedications();
+        medications = preferences.getMedications(this);
 
         txtTitle = (TextView) findViewById(R.id.ble_scan_title);
         txtTitle.setText(String.format(Locale.getDefault(), "Select device for %s", medications.get(medicationIndex).getName()));
 
+        lstDevices = (ListView) findViewById(R.id.blescan_devices);
+//        ScannedDeviceInfoAdapter adapter = (ScannedDeviceInfoAdapter) lstDevices.getAdapter();
+//        deviceName = (TextView)convertView.findViewById(com.mbientlab.bletoolbox.scanner.R.id.ble_device);
+        lstDevices.getAdapter().registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                refreshListView();
+                super.onChanged();
+            }
+        });
+
         getApplicationContext().bindService(new Intent(this, BtleService.class), this, BIND_AUTO_CREATE);
+    }
+
+    private void refreshListView(){
+        for (int index = 0; index < lstDevices.getAdapter().getCount(); index++){
+            ScannedDeviceInfo deviceInfo = (ScannedDeviceInfo) lstDevices.getAdapter().getItem(index);
+            String address = deviceInfo.btDevice.getAddress();
+            for (Medication medication : medications){
+                if (addressMapping.containsKey(medication)){
+                    if (addressMapping.get(medication).equals(address)){
+                        if (lstDevices.getChildAt(index) != null) {
+                            Log.i(TAG, address + ", " + index);
+                            // setClickable(false) and setEnabled(false) do not work here:
+                            View listViewItem = lstDevices.getChildAt(index);
+                            listViewItem.setOnClickListener(view -> Toast.makeText(SelectDeviceActivity.this,
+                                    "This device is already associated with " + medication.getName() +  ".",
+                                    Toast.LENGTH_LONG).show());
+                            listViewItem.setBackgroundColor(Color.rgb(235,235,228));
+                            TextView deviceAddress = (TextView) listViewItem.findViewById(com.mbientlab.bletoolbox.scanner.R.id.ble_mac_address);
+                            TextView deviceName = (TextView) listViewItem.findViewById(com.mbientlab.bletoolbox.scanner.R.id.ble_device);
+                            TextView deviceRSSI = (TextView) listViewItem.findViewById(com.mbientlab.bletoolbox.scanner.R.id.ble_rssi_value);
+                            deviceAddress.setTextColor(Color.rgb(155,155,140));
+                            deviceName.setTextColor(Color.rgb(155,155,140));
+                            deviceRSSI.setTextColor(Color.rgb(155,155,140));
+                        }
+                    }
+                }
+            }
+        }
     }
 
     @Override
@@ -80,17 +153,103 @@ public class SelectDeviceActivity extends AppCompatActivity implements ScannerCo
         getApplicationContext().unbindService(this);
     }
 
+    public static Task<Void> reconnect(final MetaWearBoard board) {
+        return board.connectAsync()
+                .continueWithTask(task -> {
+                    if (task.isFaulted()) {
+                        return reconnect(board);
+                    } else if (task.isCancelled()) {
+                        return task;
+                    }
+                    return Task.forResult(null);
+                });
+    }
+
+    private void configureDevice(final BluetoothDevice btDevice){
+        mwBoard= serviceBinder.getMetaWearBoard(btDevice);
+        final ProgressDialog connectDialog = new ProgressDialog(this);
+        connectDialog.setTitle(getString(R.string.title_connecting));
+        connectDialog.setMessage(getString(R.string.message_wait));
+        connectDialog.setCancelable(false);
+        connectDialog.setCanceledOnTouchOutside(false);
+        connectDialog.setIndeterminate(true);
+        connectDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.label_cancel),
+                (dialogInterface, i) -> mwBoard.disconnectAsync());
+        connectDialog.show();
+
+        mwBoard.connectAsync()
+                .continueWithTask(task -> {
+                    if (task.isCancelled()) {
+                        return task;
+                    }
+                    return task.isFaulted() ? reconnect(mwBoard) : Task.forResult(null);
+                })
+                .continueWith(task -> {
+                    if (!task.isCancelled()) {
+                        SelectDeviceActivity.this.runOnUiThread(connectDialog::dismiss);
+                        SelectDeviceActivity.this.startMotionDetection();
+                        // TODO configure device
+                    }
+                    return null;
+                });
+    }
+
+    /**
+     * Called when motion is detected on the Metawear board.
+     */
+    private void onMotionDetected(){
+        ledModule.play();
+//        iBeaconModule.configure().major((short) 1);
+//        iBeaconModule.enable();
+        // TODO : Enable then disable, e.g. after a couple of beacons
+    }
+
+    /** Module that handles motion events of the Metawear board. **/
+    private AccelerometerBosch motionModule;
+
+    /**
+     * Configures motion detection on the Metawear board.
+     */
+    private void startMotionDetection(){
+        ledModule = mwBoard.getModule(Led.class);
+        motionModule = mwBoard.getModule(AccelerometerBosch.class); // TODO is this correct sensor?
+        timer = mwBoard.getModule(Timer.class);
+        iBeaconModule = mwBoard.getModule(IBeacon.class);
+
+//        timer.scheduleAsync(30000, false, () -> iBeaconModule.configure().major((short) 0));
+        timer.scheduleAsync(5000, false, () -> ledModule.stop(false));
+        iBeaconModule.configure() // TODO
+//                .uuid(uuid)
+//                .major(major)
+//                .minor(minor)
+                .period((short)2000)
+//                .rxPower(rxPower)
+//                .txPower(txPower)
+                .commit();
+
+        ledModule.stop(true);
+        ledModule.editPattern(Led.Color.BLUE, Led.PatternPreset.SOLID).commit();
+
+        final AccelerometerBosch.AnyMotionDataProducer motionDetection = motionModule.motion(AccelerometerBosch.AnyMotionDataProducer.class);
+        // https://mbientlab.com/androiddocs/latest/data_route.html#reaction
+        motionDetection.addRouteAsync(source -> source.react(token -> onMotionDetected()))
+                .continueWith((Continuation<Route, Void>) task -> {
+                    motionDetection.start();
+                    motionModule.start();
+                    mwBoard.disconnectAsync();
+                    return null;
+                });
+    }
+
     @Override
     public void onDeviceSelected(final BluetoothDevice device) {
-        Map<Medication, String> addressMapping = preferences.getAddressMapping();
-        if (addressMapping == null){
-            addressMapping = new HashMap<>();
-        }
+        configureDevice(device);
         addressMapping.put(medications.get(medicationIndex), device.getAddress());
-        preferences.setAddressMapping(addressMapping);
+        refreshListView();
         medicationIndex++;
         if (medicationIndex >= medications.size()) {
             finish();
+            preferences.setAddressMapping(this, addressMapping);
         } else {
             txtTitle.setText(String.format(Locale.getDefault(), "Select device for %s", medications.get(medicationIndex).getName()));
         }
@@ -98,7 +257,7 @@ public class SelectDeviceActivity extends AppCompatActivity implements ScannerCo
 
     @Override
     public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-        BtleService.LocalBinder serviceBinder = (BtleService.LocalBinder) iBinder;
+        serviceBinder = (BtleService.LocalBinder) iBinder;
     }
 
     @Override
